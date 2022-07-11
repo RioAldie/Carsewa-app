c954baacb54de4982d29ad70e94a6753fd1f2a28
rioaldie <rioaldierwanto@gmail.com>
2022-07-12 04:55:11 +0700

    try add new schema in sanity.io

---
+++ sanity-backend/schemas/car.js
@@ -0,0 +1,27 @@
+export default{
+    name: 'car',
+    title: 'car',
+    type: 'document',
+    fields:[
+        {
+            name: 'name',
+            title: 'Car name',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Car image',
+            type: 'string',
+        },
+        {
+            name: 'carId',
+            title: 'CarId',
+            type: 'string',
+        },
+        {
+            name: 'cost',
+            title: 'Car Rental Cost',
+            type: 'number',
+        }
+    ]
+}
