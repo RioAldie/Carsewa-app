e0ff66cc4717409eb7a4c8931e903fdf11bb7fa8
rioaldie <rioaldierwanto@gmail.com>
2022-07-22 19:56:15 +0700

    redesign navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,80 +1,59 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { alpha, InputBase, styled } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import CarRentalIcon from '@mui/icons-material/CarRental';
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
-const Navbar = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          > <CarRentalIcon/>
-            <Typography>carsewa</Typography>
-          </IconButton>
-          {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search> */}
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+import { AppBar, Box, List, ListItemButton, styled, Toolbar, Typography } from "@mui/material";
+import Image from "next/image";
+import MenuOpenRoundedIcon from '@mui/icons-material/MenuOpenRounded';
+
+export default function Navbar () {
+
+  const Navtext = styled(Typography)({
+    color:'#000',
+    fontFamily:'roboto',
+    fontStyle:'normal',
+    fontWeight:'bold',
+    fontSize:'14px'
+  })
+  return(
+    <>
+      <Box>
+        <AppBar sx={{bgcolor:'#fff'}}>
+          <Toolbar sx={{display: 'flex', justifyContent:'space-between'}}>
+            <Box>
+              <Image src={'/icons/carsewa-logo.svg'} height={30} width={103}/>
+            </Box>
+            <Box sx={{display:{xs:'none',sm:'none',md:'block'}}}>
+              <List sx={{display:'flex', flexDirection:'row'}}> 
+                  <ListItemButton >
+                      <Navtext>
+                        Become a renter
+                      </Navtext>
+                  </ListItemButton>
+                  <ListItemButton>
+                      <Navtext>
+                        Renter Deals
+                      </Navtext>
+                  </ListItemButton>
+                  <ListItemButton>
+                      <Navtext>
+                        How it Works
+                      </Navtext>
+                  </ListItemButton>
+                  <ListItemButton>
+                      <Navtext>
+                        Why chosen us
+                      </Navtext>
+                  </ListItemButton>
+                  <ListItemButton>
+                      <Navtext sx={{color:'#EB1D36',fontSize:'20px'}}>
+                        Login
+                      </Navtext>
+                  </ListItemButton>
+              </List>
+            </Box>
+            <Box sx={{display:{xs:'block',sm:'block',md:'none'}}}>
+              <MenuOpenRoundedIcon fontSize="large" sx={{color:'#EB1D36'}}/>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   )
 }
-
-export default Navbar;
